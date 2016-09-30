36fb65dbf5eb49219a200d1c30a19ea33cfdd956
Shana White <vandersm@mail.uc.edu>
2016-09-30 15:08:21 -0400

    Fixes for BiocCheck

--- R/KO_data.R
+++ R/KO_data.R
@@ -1,8 +1,11 @@
 #' @name KO_data
 #' @title LINCS knock-out data
-#' @description This data set contains consensus genome signatures (CGS) that are the result of experimental knock-out studies conducted by the BROAD Institute.
+#' @description This data set contains consensus genome signatures (CGS) 
+#' that are the result of experimental knock-out studies conducted by the 
+#' BROAD Institute.
 #' @docType data
 #' @usage data(KO_data)
+#' @return A data.frame object
 #' @format A data frame with 36720 observations on the following 9 variables.
 #'\itemize{
 #'\item{\code{cell_id}}  {a character vector}
--- R/gene_cell_info.R
+++ R/gene_cell_info.R
@@ -1,8 +1,11 @@
 #' @name gene_cell_info
 #' @title Baseline expression information for genes across cell lines
-#' @description This data set contains baseline expression measurements for genes in cell lines [without any experimental perturbations] as profiles by LINCS
+#' @description This data set contains baseline expression measurements for 
+#' genes in cell lines [without any experimental perturbations] as profiled
+#' by the LINCS data consortium
 #' @docType data
 #' @usage data(gene_cell_info)
+#' @return A data.frame object
 #' @format A data frame with 1703457 observations on the following 6 variables.
 #'\itemize{
 #'\item{\code{pr_gene_symbol}}{a character vector}
